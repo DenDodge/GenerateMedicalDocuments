df94217d6c0911d0192a224ced68c7424cb90506
Denis Nestruev <dnestruev@syntellect.ru>
2022-07-07 22:19:13 +0300

    add easyDocs library

---
+++ EasyDocs/ErrorToRussianString.cs
@@ -0,0 +1,15 @@
+﻿namespace EasyDox
+{
+    public class ErrorToRussianString : IMergeErrorVisitor
+    {
+        string IMergeErrorVisitor.InvalidExpression(string expr)
+        {
+            return "Ошибка в выражении: " + expr + ".";
+        }
+
+        string IMergeErrorVisitor.MissingField(string fieldName)
+        {
+            return "Не заполнено поле " + fieldName + ".";
+        }
+    }
+}
---
+++ EasyDocs/ExpressionEvaluator.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+
+namespace EasyDox
+{
+    class ExpressionEvaluator : IExpressionVisitor
+    {
+        private readonly Properties properties;
+
+        string result;
+
+        public ExpressionEvaluator (Properties properties, IExpression expression)
+        {
+            this.properties = properties;
+
+            expression.Accept (this);
+        }
+
+        public string Result
+        {
+            get { return result; }
+        }
+
+        void IExpressionVisitor.Visit(Literal literal)
+        {
+            result = literal.Value;
+        }
+
+        void IExpressionVisitor.Visit(Field field)
+        {
+            result = properties [field.Name];
+        }
+
+        void IExpressionVisitor.Visit(Function function)
+        {
+            result = (string) function.Definition.DynamicInvoke(function.Args.Select (a => (object) new ExpressionEvaluator (properties, a).result).ToArray());
+        }
+    }
+}
